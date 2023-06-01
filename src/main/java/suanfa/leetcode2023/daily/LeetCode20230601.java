98df0dab7a484e41f36fde09bc3673ef4d6b593e
fntp <stickpoint@163.com>
2023-06-01 17:42:15 +0800

    优化算法

--- src/main/java/suanfa/leetcode2023/daily/LeetCode20230601.java
+++ src/main/java/suanfa/leetcode2023/daily/LeetCode20230601.java
@@ -10,11 +10,17 @@ import java.util.Objects;
 public class LeetCode20230601 {
 
     public static void main(String[] args) {
-        int[] nums = {1};
-        int[] ints = searchRange(nums, 1);
+        int[] nums = {1,2,2,3,4,4,4};
+        int[] ints = searchRange(nums, 4);
         System.out.println(Arrays.toString(ints));
     }
 
+    /**
+     * 最优解：双指针 找到就立马返回 不作多余统计
+     * @param nums
+     * @param target
+     * @return
+     */
     public static int[] searchRange(int[] nums, int target) {
         int[] addressInt = new int[]{-1,-1};
         if (nums.length==0){
@@ -30,14 +36,20 @@ public class LeetCode20230601 {
         // 初始化两个指针
         int leftIndex = 0, rightIndex = nums.length - 1;
         // 然后计算
-        while (leftIndex<nums.length - 1&&rightIndex>0){
+        while (leftIndex<nums.length - 1 && rightIndex>0){
             if (addressInt[0]==-1 && nums[leftIndex] == target){
                 addressInt[0] = leftIndex;
+                if (addressInt[1]!=-1){
+                    return addressInt;
+                }
             }else {
                 leftIndex++;
             }
             if (addressInt[1]==-1 && nums[rightIndex]==target){
                 addressInt[1] = rightIndex;
+                if (addressInt[0]!=-1){
+                    return addressInt;
+                }
             }else {
                 rightIndex--;
             }
