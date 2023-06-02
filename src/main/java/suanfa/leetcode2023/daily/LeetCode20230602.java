0ab188c5b334c29bdc6336d5b240e8c9c0009cb2
fntp <stickpoint@163.com>
2023-06-02 15:07:52 +0800

    0602算法

--- src/main/java/suanfa/leetcode2023/daily/LeetCode20230602.java
+++ src/main/java/suanfa/leetcode2023/daily/LeetCode20230602.java
@@ -35,4 +35,41 @@ public class LeetCode20230602 {
         }
         return -1;
     }
+
+    public static void main(String[] args) {
+        LeetCode20230602 leetCode20230602 = new LeetCode20230602();
+        System.out.println(leetCode20230602.search1(new int[]{-1, 0, 3, 5, 9, 12}, 2));
+    }
+
+    public int search1(int[] nums, int target) {
+        // 先去界定边界问题
+        if (nums.length ==0){
+            return -1;
+        }
+        if (nums.length==1){
+            return nums[0]==target ? 0 : -1;
+        }
+        if (nums[0]==target){
+            return 0;
+        }
+        if (nums[nums.length-1]==target){
+            return nums.length-1;
+        }
+        // 初始值
+        int rightIndex = nums.length-1;
+        int leftIndex = 0;
+        while (leftIndex<=rightIndex){
+            int middleIndex = (leftIndex+rightIndex)/2;
+            if(nums[middleIndex]==target){
+                return middleIndex;
+            }
+            if (nums[middleIndex]>target){
+                rightIndex = middleIndex-1;
+            }else {
+                leftIndex = middleIndex+1;
+            }
+        }
+        return -1;
+    }
+
 }
---
+++ src/main/java/suanfa/leetcode2023/daily/LeetCode20230602_3.java
@@ -0,0 +1,35 @@
+package suanfa.leetcode2023.daily;
+
+/**
+ * @author puye(0303)
+ * @since 2023/6/2
+ */
+public class LeetCode20230602_3 {
+
+    public static void main(String[] args) {
+        LeetCode20230602_3 leetCode202306022 = new LeetCode20230602_3();
+        System.out.println(leetCode202306022.searchInsert(new int[]{1}, 0));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        if (nums.length==0) {
+            return -1;
+        }
+        if (nums.length==1){
+            if (nums[0]>=target){
+                return 0;
+            }else {
+                return nums.length;
+            }
+        }
+        int leftIndex = 0;
+        while (leftIndex<=nums.length-1){
+            if (nums[leftIndex]<target){
+                leftIndex++;
+            }else {
+                return leftIndex;
+            }
+        }
+        return nums.length;
+    }
+}
