a2fcdf497376e0bce165fab46b332177a42a10f5
fntp <fntp66@gmail.com>
2025-02-19 14:28:01 +0800

    ArrayList底层回顾

--- src/main/java/com/study/TestXC.java
+++ src/main/java/com/study/TestXC.java
@@ -1,5 +1,12 @@
 package com.study;
 
+import jdk.internal.util.ArraysSupport;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @author fntp
  * @date 2025/1/16
@@ -8,7 +15,20 @@ package com.study;
 public class TestXC {
 
     public static void main(String[] args) {
-
+        //HashMap<String,String> map1 = new HashMap<>();
+        //ConcurrentHashMap<String,String> map = new ConcurrentHashMap<>();
+        //map.put(null,null);
+        //map1.put(null,"scx");
+        //System.out.println(map1.get(null));
+        //System.out.println(map.get(null));
+        System.out.println(10 >> 1);
+        //int newCapacity = ArraysSupport.newLength(10,
+        //        11 - 10, /* minimum growth */
+        //        10 >> 1);
+        //System.out.println(newCapacity);
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.ensureCapacity(100);
+        //System.out.println(integers);
     }
 
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author fntp
+ * @date 2025/2/19
+ * @apiNote
+ */module dailyStudy {
+     requires java.base;
+     requires java.xml;
+}
